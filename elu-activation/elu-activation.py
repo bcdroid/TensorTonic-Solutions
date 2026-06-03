d304a10affc09d213573a0bd147b17cf0bea37ec
bcdroid <842930+bcdroid@users.noreply.github.com>
2026-06-03 12:23:38 -0500

    Add solution for elu-activation

---
+++ elu-activation/elu-activation.py
@@ -0,0 +1,13 @@
+import math
+
+def elu(x, alpha):
+    """
+    Apply ELU activation to each element.
+    """
+    result = []
+    for i in range(len(x)):
+        if x[i] > 0:
+            result.append (x[i])
+        else:
+            result.append (alpha * (math.exp(x[i]) - 1))
+    return result
